9e6e0dd83eeb4f8999b5ca6d63ca3c59c64db7d1
Bulin Alexey <bulin92@inbox.ru>
2023-09-16 18:40:10 +0700

    testFailed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Vasiliy"))
+                .body("data", equalTo("Valera"))
         ;
     }
 
